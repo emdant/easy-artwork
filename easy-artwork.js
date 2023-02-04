3d41937086a31372c8d45067772ecc0f0a2b69b4
emdant <30555419+emdant@users.noreply.github.com>
2023-02-04 11:52:01 +0100

    Small fix

--- easy-artwork.js
+++ easy-artwork.js
@@ -4,13 +4,12 @@ function addButton(sheet, buttons) {
 		icon: 'far fa-images',
 		label: 'Show Artwork',
 		onclick: (e) => {
-			const [type, id] = sheet.id.split('-');
-			const entity = game[`${type}s`].get(id);
+			const doc = sheet.document;
 
 			game.socket.emit('shareImage', {
-				image: entity.data.img,
-				title: "",
-				uuid: entity.uuid
+				image: doc.img,
+				title: doc.name,
+				uuid: doc.uuid
 			});
 		}
 	});
